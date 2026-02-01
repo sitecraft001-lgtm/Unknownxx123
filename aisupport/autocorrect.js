8558392d901ce3a78d463475710b35ec2297f8c5
sitecraft001-lgtm <sitecraft001@gmail.com>
2026-02-01 21:13:52 +0530

    Add files via upload

---
+++ aisupport/autocorrect.js
@@ -0,0 +1,23 @@
+// Wrong -> Correct words
+const autocorrect = [
+  {
+    correct: "javascript",
+    wrong: ["javscrit", "javascrit", "javscript", "java script", "javasript", "js"]
+  },
+  {
+    correct: "html",
+    wrong: ["htlm", "hmtl", "hypertext", "hyper text"]
+  },
+  {
+    correct: "css",
+    wrong: ["cs", "ccs", "style", "styling"]
+  },
+  {
+    correct: "hello",
+    wrong: ["helo", "hii", "hey", "helloo"]
+  },
+  {
+    correct: "error",
+    wrong: ["eror", "err", "bug", "issue", "mistke"]
+  }
+];
---
+++ aisupport/commands.js
@@ -0,0 +1,9 @@
+// All commands and responses
+const commands = {
+  "hello": "Hello 👋 I'm Code Zing AI.",
+  "html": "HTML is used to create website structure.",
+  "css": "CSS styles the website.",
+  "javascript": "JavaScript adds logic and interaction.",
+  "error": "Paste your error, I will debug it.",
+  "price": "Website pricing depends on features."
+};
