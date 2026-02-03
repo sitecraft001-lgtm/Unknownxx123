7850f04e133d5b905d5112fbede4f04db13e14aa
sitecraft001-lgtm <sitecraft001@gmail.com>
2026-02-03 20:18:55 +0530

    Create 671.js

---
+++ snippets/671.js
@@ -0,0 +1,21 @@
+from turtle import *
+import colorsys
+
+speed(0)
+pensize(2)
+bgcolor("black")
+h = 0.0
+
+for i in range(100):
+    r, g, b = colorsys.hsv_to_rgb(h, 1, 1)
+    color(r, g, b)
+    h += 0.01  
+
+    for j in range(4):
+        fd(i)
+        rt(20)
+        rt(60)
+
+    rt(120)
+
+done()
