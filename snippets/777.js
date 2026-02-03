a99e52565ee9a5d2bb7eab81a696b6c1ce2b502b
sitecraft001-lgtm <sitecraft001@gmail.com>
2026-02-03 21:47:30 +0530

    Update 777.js

--- snippets/777.js
+++ snippets/777.js
@@ -1,21 +1,12 @@
-SNIPPETS.push({
+SNIPPETS["777"] = {
   id: "777",
   title: "Turtle Star",
   code: `
-from turtle import *
-from colorsys import *
-
-tracer(20)
-bgcolor("black")
-h = 0
-pensize(2)
-speed(0)
-
-for i in range(6):
-    h += 0.009
-    color(h,1,1)
-    forward(200)
-    left(144)
-`,
-  tags: ["python", "turtle", "graphics"]
-});
+import turtle
+t = turtle.Turtle()
+for i in range(5):
+    t.forward(100)
+    t.right(144)
+turtle.done()
+`
+};
